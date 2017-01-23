8d920622458f03a0f6c2c7769824dd8df4171749
Ilya Petrash <ilya.petrash@inbox.ru>
2017-01-24 00:35:30 +0500

    Добавление меню с ссылками на сайт манги Фильтрация клика ссылок, чтобы не открывались рекламные страницы

--- app/src/main/java/com/example/ipetrash/webview_readmanga/MainActivity.java
+++ app/src/main/java/com/example/ipetrash/webview_readmanga/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.ipetrash.webview_readmanga;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+
+    private String urlGrouple = "http://grouple.ru/";
+    private String urlReadmanga = "http://readmanga.me/";
+    private String urlMintmanga = "http://mintmanga.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,23 +22,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Фильтр ссылок -- позволяем открывать только те, что описаны ниже
+                String host = Uri.parse(url).getHost();
+                if(host.endsWith("grouple.ru") || host.endsWith("readmanga.me") || host.endsWith("mintmanga.com")) {
+                    return false;
+                }
 
-        SimpleWebViewClientImpl webViewClient = new SimpleWebViewClientImpl(this);
-        webView.setWebViewClient(webViewClient);
+                // Игнорирование других ссылок (на том сайте открывалась редиректом рекдама на весь экран
+                // что вызывало боль, т.к. ни закрыть ее, ни вернуться назад не получалось)
+                return true;
+            }
+        });
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        webView.loadUrl("http://readmanga.me/");
+        webView.loadUrl(urlReadmanga);
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
             webView.goBack();
-            return true;
+        } else {
+            super.onBackPressed();
         }
+    }
 
-        return super.onKeyDown(keyCode, event);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_grouple:
+                webView.loadUrl(urlGrouple);
+                return true;
+
+            case R.id.action_readmanga:
+                webView.loadUrl(urlReadmanga);
+                return true;
+
+            case R.id.action_mintmanga:
+                webView.loadUrl(urlMintmanga);
+                return true;
+
+            case R.id.action_reload:
+                webView.reload();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
